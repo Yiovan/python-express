fac0f67a4efbc5285199c7a820f459e69970d148
Giovanni <giovannicabrerarivas@icloud.com>
2025-06-28 13:26:34 -0300

    entradas

---
+++ Clase2/entrada.py
@@ -0,0 +1,10 @@
+
+
+
+cadena = imput ("ingrese nombre de su proyecto:")
+
+print (f "su proyecto se llama: "{cadena})
+
+numero = int(input("ingrese un numero: "))
+print(f"El numero ingresado es: {numero}")
+
---
+++ Clase2/logicos.py
@@ -0,0 +1,13 @@
+'''
+and
+or
+not 
+'''
+edad= 17
+if (edad>=0 or edad<=18):
+    print("Es menor de edad")
+else:
+    print("Es mayor de edad")
+    
+    
+    
