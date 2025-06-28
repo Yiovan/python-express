d605bb7fa03a9b90197333f0ee54ff8f3596862e
Giovanni <giovannicabrerarivas@icloud.com>
2025-06-28 12:45:07 -0300

    finalizado variables

---
+++ Clase2/variables.py
@@ -0,0 +1,27 @@
+'''
+esto es un comentarPariatur sit Lorem Lorem minim duis et Lorem id.
+'''
+
+# Variables en Python
+print ("hola mundo");
+
+entero = 10;
+flotante = 10.5;
+cadena = "Hola, Python";
+booleano = True;
+print(entero, flotante, cadena, booleano);
+print(type(entero), type(flotante), type(cadena), type(booleano));
+
+# Operaciones con variables 
+print("suma todas las variables");
+
+a= 10;
+b= 20;
+print("suma de a y b es: ", a + b);
+print("resta todas las variables");
+print("resta de a y b es: ", a - b);    
+print("multiplica todas las variables");
+print("multiplicacion de a y b es: ", a * b);   
+b=2
+print("divide todas las variables");
+print("division de a y b es: ", a / b);
