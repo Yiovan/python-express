8efe15d2d029ab6273edbcce06c77733b16754bb
Giovanni <giovannicabrerarivas@icloud.com>
2025-07-16 12:08:03 -0300

    api

--- Clase4/api/app.py
+++ Clase4/api/app.py
@@ -8,11 +8,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 
 def unida():
     return "Hola desde la unida!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0..0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
